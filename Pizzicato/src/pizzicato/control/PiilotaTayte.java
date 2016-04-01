45006c97fbd0bf45ff106522cc9ca408b7751709
a1500911 <a1500911@PAO5003Z08.haagahelia.amk>
2016-04-01 14:44:33 +0300

    piilotatayte piilotapizza mukaan

---
+++ Pizzicato/src/pizzicato/control/PiilotaTayte.java
@@ -0,0 +1,27 @@
+package pizzicato.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pizzicato.model.dao.TayteDAO;
+
+
+@WebServlet("/PiilotaTayte")
+public class PiilotaTayte extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idIn = request.getParameter("id");
+		int tayte_id = Integer.parseInt(idIn);
+		TayteDAO taytedao = new TayteDAO();
+		taytedao.piilotatayte(tayte_id);
+		
+		String page ="/Pizzicato/Muokkaussivu";
+		response.sendRedirect(page);
+	}
+
+}
