e3865de27976b3bfd2d441781576a08af5675f10
a1500911 <a1500911@PAO5003Z08.haagahelia.amk>
2016-04-04 10:51:00 +0300

    vaalennettu taulukkotausta vaihdettu, vanha tallessa edelleen missä olikin, uusi: taulukkotausta_vaal. muokkaussivu.java lisätty tayte-koodit ihan kuten pizza-kooditkin siellä ollut.. Jen

--- Pizzicato/src/pizzicato/control/Muokkaussivu.java
+++ Pizzicato/src/pizzicato/control/Muokkaussivu.java
@@ -13,8 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
+
+
 import pizzicato.model.Pizza;
+import pizzicato.model.Tayte;
 import pizzicato.model.dao.PizzaDAO;
+import pizzicato.model.dao.TayteDAO;
 import pizzicato.control.MuokkaaPizza;
 
 
@@ -28,13 +32,19 @@ public class Muokkaussivu extends HttpServlet {
 		// Luodaan pizzatDAO ja käsketään hakemaan kaikki jutut
 		PizzaDAO pizzaDAO = new PizzaDAO();
 		ArrayList<Pizza> Pizzat = pizzaDAO.findAll(); 
+		
+		TayteDAO tayteDAO = new TayteDAO();
+		ArrayList<Tayte> Taytteet = tayteDAO.findAll();
 
 		// Talletetaan request-olion alle henkilölista, jotta tiedot ovat
 		// käytössä jsp:llä
 		request.setAttribute("pizzat", Pizzat); 
+		request.setAttribute("taytteet", Taytteet);
 		
 		List<Pizza> piilossa = pizzaDAO.findAllHidden();
 		request.setAttribute("piilopizzat", piilossa);
+		List<Tayte> tpiilossa = tayteDAO.findAllHidden();
+		request.setAttribute("piilotaytteet", tpiilossa);
 		
 		
 		String jsp = "/view/Muokkaussivu.jsp";
@@ -66,6 +76,17 @@ public class Muokkaussivu extends HttpServlet {
 		  dispather.forward(request, response);
 		  
 		  
+		  String valitunTaytteenId=request.getParameter("tayte_id");
+		  int tid = Integer.parseInt(valitunTaytteenId);
+		  TayteDAO taytedao = new TayteDAO();
+		  ArrayList<Tayte> tayte = taytedao.findAll();
+		  
+		  Tayte valittuTayte = new Tayte();
+		  for(Tayte taytee : tayte){
+			  if(taytee.getTayte_id()==(tid)){
+				  valittuTayte = taytee;
+			  }
+		  }
 	}	
 	
 } 
